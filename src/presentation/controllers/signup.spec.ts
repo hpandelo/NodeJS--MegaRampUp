7e45174a4b85e31664d1e7d6657f31442cc813bc
Helcio Pandelo <hmacedo2007@gmail.com>
2021-06-30 00:09:11 -0300

    chore: testing

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -2,16 +2,30 @@ import { SignUpController } from './signup'
 
 describe('SignUp Controller', () => {
   test('Should return 400 if no name is provided', () => {
-    const sut = new SignUpController();
+    const sut = new SignUpController()
     const httpRequest = {
       body: {
         email: 'any@email.com',
         password: 'password',
         passwordConfirmation: 'password'
       }
-    }
+    };
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missing param: name'))
+  })
+
+  test('Should return 400 if no email is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any name',
+        password: 'password',
+        passwordConfirmation: 'password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
   })
 })
