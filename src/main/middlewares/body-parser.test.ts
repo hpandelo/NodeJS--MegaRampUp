3c6c4e1b6c76af45be284b440a248d703e967fc8
Helcio Pandelo <hmacedo2007@gmail.com>
2021-09-21 18:00:04 -0300

    feat: ensure cors is enabled

--- src/main/middlewares/body-parser.test.ts
+++ src/main/middlewares/body-parser.test.ts
@@ -4,7 +4,6 @@ import app from '../config/app'
 describe('Body Parser Middleware', () => {
   test('Should parse body as JSON', async () => {
     app.post('/test_body_parser', (req, res) => res.send(req.body))
-
     const testPayload = { name: 'test' }
 
     await request(app)
