f1c7626012361e99fa75a9484ea958118618c443
Helcio Pandelo <hmacedo2007@gmail.com>
2021-09-21 18:14:49 -0300

    test: ensure xml content-type is returned when forced

--- src/main/middlewares/content-type.test.ts
+++ src/main/middlewares/content-type.test.ts
@@ -3,12 +3,25 @@ import app from '../config/app'
 
 describe('Content-Type Middleware', () => {
   test('Should return default content-type as JSON', async () => {
-    const endpoint = '/test_content_type'
+    const endpoint = '/test_default_content_type'
 
     app.get(endpoint, (req, res) => res.send(''))
 
     await request(app)
       .get(endpoint)
       .expect('content-type', /json/)
+  })
+
+  test('Should return xml content-type when forced', async () => {
+    const endpoint = '/test_content_type_xml'
+
+    app.get(endpoint, (req, res) => {
+      res.type('xml')
+      res.send('')
+    })
+
+    await request(app)
+      .get(endpoint)
+      .expect('content-type', /xml/)
   })
 })
